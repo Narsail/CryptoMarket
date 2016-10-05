1d1d95706d2968cfcaa3af07bcf85eb501ac6bb8
David Moeller <davidmoeller@Davids-MacBook-Pro.local>
2016-10-05 10:55:40 +0200

    added explanation

--- CryptoMarket/SubRootViewController.swift
+++ CryptoMarket/SubRootViewController.swift
@@ -8,4 +8,6 @@
 
 import UIKit
 
+/* This class is necessary for the RootRouter Class (which is not in use at the moment). I couldn't use UIViewController as a Base class so i had to create this intermediate class. */
+
 class SubRootViewController: UIViewController {}
